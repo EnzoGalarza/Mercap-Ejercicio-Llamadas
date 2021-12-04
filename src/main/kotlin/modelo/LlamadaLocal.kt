dcbd227b6e0010f15796f67978a22eb91136a0c3
EnzoGalarza <EnzoGl33@hotmail.com>
2021-12-04 00:05:53 -0300

    Agrego logica de valor por tipo en llamadaLocal

--- src/main/kotlin/modelo/LlamadaLocal.kt
+++ src/main/kotlin/modelo/LlamadaLocal.kt
@@ -3,5 +3,22 @@ package modelo
 import java.time.LocalDateTime
 
 class LlamadaLocal(duracion: Int, fecha : LocalDateTime, ubicacion: Ubicacion) : Llamada(duracion,fecha,ubicacion){
+    val diasNoHabiles = listOf("SATURDAY","SUNDAY")
 
+    override fun costoPorTipo(): Double {
+        var costo = 0.10
+
+        if(esDiaHabil() && esHorarioHabil()){
+            costo = 0.20
+        }
+        return costo
+    }
+
+    private fun esDiaHabil() : Boolean{
+        return !diasNoHabiles.contains(fecha.dayOfWeek.name)
+    }
+
+    private fun esHorarioHabil() : Boolean{
+        return fecha.hour in 8..20
+    }
 }
--- src/test/kotlin/modelo/LlamadaLocalTestCase.kt
+++ src/test/kotlin/modelo/LlamadaLocalTestCase.kt
@@ -1,5 +1,6 @@
 package modelo
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 import java.time.Month
@@ -7,10 +8,29 @@ import java.time.Month
 class LlamadaLocalTestCase {
 
     @Test
-    fun unaLlamadaLocalEnDiaHabilTieneUnMontoEquivalenteA20Centavos(){
+    fun unaLlamadaLocalEnDiaYHorarioHabilesTieneUnMontoEquivalenteA20Centavos(){
         val ubicacion = Ubicacion("Argentina","Quilmes")
-        val fechaLlamada = LocalDateTime.of(2021, Month.OCTOBER,17,1,0)
+        val fechaLlamada = LocalDateTime.of(2021, Month.OCTOBER,18,9,0)
         val nuevaLlamada = LlamadaLocal(10,fechaLlamada,ubicacion)
 
+        Assertions.assertEquals(0.20,nuevaLlamada.costoPorTipo())
+    }
+
+    @Test
+    fun unaLLamadaLocalEnDiaNoHabilTieneUnMontoEquivalenteA10Centavos(){
+        val ubicacion = Ubicacion("Argentina","Quilmes")
+        val fechaLlamada = LocalDateTime.of(2021, Month.OCTOBER,17,9,0)
+        val nuevaLlamada = LlamadaLocal(10,fechaLlamada,ubicacion)
+
+        Assertions.assertEquals(0.10,nuevaLlamada.costoPorTipo())
+    }
+
+    @Test
+    fun unaLlamadaEnHorarioNoHabilTieneUnMontoEquivalenteA10Centavos(){
+        val ubicacion = Ubicacion("Argentina","Quilmes")
+        val fechaLlamada = LocalDateTime.of(2021, Month.OCTOBER,18,2,0)
+        val nuevaLlamada = LlamadaLocal(10,fechaLlamada,ubicacion)
+
+        Assertions.assertEquals(0.10,nuevaLlamada.costoPorTipo())
     }
 }
